e7dea71f8f2a4cb0549f8c0b6f490c088160f828
Genie <hellogenie97@gmail.com>
2020-03-01 01:40:45 +0900

    categorization

--- 200227_200301_문제풀이/BOJ1987_알파벳_Main_임효진.java
+++ 200227_200301_문제풀이/BOJ1987_알파벳_Main_임효진.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ1987_알파벳 {
+public class BOJ1987_알파벳_Main_임효진 {
 
 	public static char[][] map;
 	public static boolean[] alpha;
